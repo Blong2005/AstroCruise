c05de00ea45de8b46fce4276c459abcee678261b
ColdAndromeda <bensonzheng87901@gmail.com>
2023-01-10 18:27:28 -0500

    Added more to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        trispace: "'Trispace', serif"
+      }
+    },
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
 }
