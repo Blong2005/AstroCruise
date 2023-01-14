64d0e6a5dc07d605ee505b1428e31142d1f6c1ff
ColdAndromeda <bensonzheng87901@gmail.com>
2023-01-13 22:21:55 -0500

    Formated all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        trispace: "'Trispace', serif"
+        trispace: "'Trispace', serif",
       },
       backgroundImage: {
-        'rocket' : 'url("https://github.com/ColdAndromeda/AstroCruise/blob/main/dist/Images/SpaceX-Falcon-9-launch-in-night-sky.jpg?raw=true")'
-      }
+        rocket:
+          'url("https://github.com/ColdAndromeda/AstroCruise/blob/main/dist/Images/SpaceX-Falcon-9-launch-in-night-sky.jpg?raw=true")',
+      },
     },
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
-}
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+};
